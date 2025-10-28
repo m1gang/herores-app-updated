a365947998035b6e335ad448df25a2e04241a0f3
MiGaNg <miguelangelyv1@gmail.com>
2025-10-28 11:39:56 -0500

    Componente Layout para heroes y admin

--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,4 +1,6 @@
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { SearchPage } from "@/heroes/pages/search/SearchPage";
@@ -7,19 +9,33 @@ import { createBrowserRouter } from "react-router";
 export const appRouter = createBrowserRouter([
     {
         path: '/',
-        element: <HomePage />
-    },
-    {
-        path: '/hero/1',
-        element: <HeroPage />
-    },
-    {
-        path: '/search',
-        element: <SearchPage />
+        element: <HeroesLayout />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: 'hero/1',
+                element: <HeroPage />
+            },
+            {
+                path: 'search',
+                element: <SearchPage />
+            },
+
+        ],
     },
     {
         path: '/admin',
-        element: <AdminPage />
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <AdminPage />
+            }
+        ]
     },
+
 
 ])
