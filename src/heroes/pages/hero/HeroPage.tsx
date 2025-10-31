19478681c338f225ef49773bf18a2e98337d8ab6
MiGaNg <miguelangelyv1@gmail.com>
2025-10-31 17:30:45 -0500

    Navegación manual dentro de las tarjetas

--- src/heroes/pages/hero/HeroPage.tsx
+++ src/heroes/pages/hero/HeroPage.tsx
@@ -1,6 +1,15 @@
+import { useParams } from "react-router"
 
 export const HeroPage = () => {
+
+    const { idSlug = '' } = useParams();
+    console.log({ idSlug });
+
+
     return (
-        <div>HeroPage</div>
+        <>
+            <div>HeroPage</div>
+            <h1 className="text-5xl">{idSlug}</h1>
+        </>
     )
 }
--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
@@ -21,13 +21,17 @@ export const appRouter = createBrowserRouter([
                 element: <HomePage />
             },
             {
-                path: 'hero/1',
+                path: 'heroes/:idSlug',
                 element: <HeroPage />
             },
             {
                 path: 'search',
                 element: <SearchPage />
             },
+            {
+                path: '*',
+                element: <Navigate to='/' />
+            }
 
         ],
     },
