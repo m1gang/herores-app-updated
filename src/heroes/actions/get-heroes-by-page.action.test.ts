0ba05d8c0b6a10afa90e5b755f2ad86311a0773f
MiGaNg <miguelangelyv1@gmail.com>
2025-11-10 10:52:54 -0500

    Parte 2 - Test - getHeroesByPageAction

--- src/heroes/actions/get-heroes-by-page.action.test.ts
+++ src/heroes/actions/get-heroes-by-page.action.test.ts
@@ -54,10 +54,45 @@ describe('getHeroesByPageAction', () => {
 
         await getHeroesByPageAction('abc' as unknown as number)
 
-        const request = heroesApiMock.history;
-        console.log(heroesApiMock.history);
-
-
+        const params = heroesApiMock.history.get[0].params;
+        expect(params).toStrictEqual(
+            { limit: 6, offset: 0, category: 'all' }
+        )
     })
 
+    test('should return the correct heroes when page is a string number', async () => {
+        const responseObject = {
+            total: 10,
+            pages: 1,
+            heroes: []
+        }
+
+        heroesApiMock.onGet('/').reply(200, responseObject);
+
+        await getHeroesByPageAction('5' as unknown as number)
+
+        const params = heroesApiMock.history.get[0].params;
+        expect(params).toStrictEqual(
+            { limit: 6, offset: 24, category: 'all' }
+        )
+    })
+
+    test('should call the api with correct params', async () => {
+        const responseObject = {
+            total: 10,
+            pages: 1,
+            heroes: []
+        }
+
+        heroesApiMock.onGet('/').reply(200, responseObject);
+
+        await getHeroesByPageAction(2, 10, 'heroes')
+
+        const params = heroesApiMock.history.get[0].params;
+        console.log(params);
+
+        expect(params).toStrictEqual(
+            { limit: 10, offset: 10, category: 'heroes' }
+        )
+    })
 })
