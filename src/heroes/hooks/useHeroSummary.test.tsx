0944a7fa9bb627375e398578f49bf2ffc2e67bf3
MiGaNg <miguelangelyv1@gmail.com>
2025-11-11 10:54:55 -0500

    Parte 2 - Test - useHeroSummary

--- src/heroes/hooks/useHeroSummary.test.tsx
+++ src/heroes/hooks/useHeroSummary.test.tsx
@@ -1,8 +1,17 @@
-import { describe, expect, test } from "vitest";
-import { renderHook } from '@testing-library/react'
+import { describe, expect, test, vi } from "vitest";
+import { renderHook, waitFor } from '@testing-library/react'
 import { useHeroSummary } from "./useHeroSummary";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Children, type PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
+import { getSummaryAction } from "../actions/get-summary.action";
+import type { SummaryInformationResponse } from "../types/summary-information.response";
+
+
+vi.mock('../actions/get-summary.action', () => ({
+    getSummaryAction: vi.fn(),
+}))
+
+const mockGetSummaryAction = vi.mocked(getSummaryAction)
 
 const tanstackCustomProvider = () => {
 
@@ -30,5 +39,36 @@ describe('useHeroSummary', () => {
         expect(result.current.data).toBe(undefined);
 
     })
+
+    test('should return success state with data when API call success', async () => {
+
+
+        const mockSummaryData = {
+            totalHeroes: 10,
+            strongestHero: {
+                id: '1',
+                name: 'Superman'
+            },
+            smartestHero: {
+                id: '2',
+                name: 'Batman'
+            },
+            heroCount: 18,
+            villainCount: 7
+        } as SummaryInformationResponse
+
+        mockGetSummaryAction.mockResolvedValue(mockSummaryData);
+
+        const { result } = renderHook(() => useHeroSummary(), {
+            wrapper: tanstackCustomProvider()
+        });
+
+        await waitFor(() => {
+            expect(result.current.isSuccess).toBe(true);
+        })
+
+        expect(result.current.isError).toBe(false)
+    })
+
 
 })
